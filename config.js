946e152341bfe7aca1a9519bcd0d17954d2b83b1
171037 <150212991+171037@users.noreply.github.com>
2024-01-24 22:57:51 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const { getAuth, setPersistence, signInWithEmailAndPassword, browserSessionPersi
 const { getStorage, ref, uploadBytes, getDownloadURL} = require("firebase/storage");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAbsnrv1V48Afy3kFGF5FjQ7qe2NPWkgKQ",
-    authDomain: "home-ebddf.firebaseapp.com",
-    projectId: "home-ebddf",
-    databaseURL:"https://home-ebddf-default-rtdb.firebaseio.com/",
-    storageBucket: "home-ebddf.appspot.com",
-    messagingSenderId: "701662885710",
-    appId: "1:701662885710:web:00cb728e62209719e8501f",
-    measurementId: "G-4L5HSMC0ZZ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    databaseURL:"",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 const app = initializeApp(firebaseConfig);
